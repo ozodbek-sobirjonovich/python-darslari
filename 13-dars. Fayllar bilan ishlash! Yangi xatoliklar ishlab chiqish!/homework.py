cd8736355170c551586301e46f20d0129135b4e7
Ozodbek Sobirjonovich | Software Developer <117828158+ozodbek-sobirjonovich@users.noreply.github.com>
2023-05-04 10:36:59 +0500

    homework.py

--- 13-dars. Fayllar bilan ishlash! Yangi xatoliklar ishlab chiqish!/homework.py
+++ 13-dars. Fayllar bilan ishlash! Yangi xatoliklar ishlab chiqish!/homework.py
@@ -1,24 +1,23 @@
 """ TOPSHIRIQLAR """
 
 ################################################################################
-# 1-TOPSHIRIQ. input.txt fayliga foydalanuvchi tomonidan 6 ta son orasida bo'shliqlar bilan yozishi kerak!. Ularning ichidan JUFT sonlarini bir-biriga qo'shib, natijani output.txt fayliga yozing!
-
+# 1-TOPSHIRIQ. input.txt fayliga foydalanuvchi tomonidan 6 ta son orasida bo'shliqlar bilan yozishi kerak!.
+# Ularning ichidan JUFT sonlarini bir-biriga qo'shib, natijani output.txt fayliga yozing!
 # Masalan: input.txt faylida 1 2 3 4 5 6 bo'lsa, output.txt fayliga 2 + 4 + 6 = 12 yozilishi kerak!
 ################################################################################
 
-###############################################################################
+
+################################################################################
 # 2-TOPSHIRIQ. input.txt fayliga bitta bo'shliq bilan ajratilgan holatda sonlar yozilgan. Ularning ichidan yurib chiqib, har birini o'zining darajasiga oshirib, natijani output.txt fayliga yozing!
-
 # Masalan: input.txt faylida 1 2 3 4 5 6 bo'lsa, output.txt fayliga 1 4 27 256 3125 46656 yozilishi kerak!
-###############################################################################
+################################################################################
 
-###############################################################################
+
+################################################################################
 # 3-TOPSHIRIQ. A+B. Ikkita butun son A va B ning yig'indisini hisoblang.
-
 # Kiruvchi ma'lumotlar:
 # INPUT.TXT kirish faylining alohida qatorlarida ikkita manfiy bo'lmagan butun sonlar berilgan, sonlar 10ning 100-darajasidan oshmaydi.
-
 # Chiquvchi ma'lumotlar:
 # OUTPUT.TXT chiqish faylining yagona satrida berilgan ikki sonning yig'indisini(boshlang'ich nollarsiz) chiqaring.
-
 # Masalan: INPUT.TXT faylida 5 va 12 bo'lsa, OUTPUT.TXT fayliga 17 yozilishi kerak.
+################################################################################
